5fde7b9963ad13b28661617c65f0eb6017057121
xosebastian <sebas.sm.saac@gmail.com>
2017-08-11 07:29:56 -0300

    Iniciamos

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".btn-search").click(function(){
+        $( ".navbar-nav li:first" ).addClass( "hide" );
+        $( ".search-form" ).show();        $("#search-input").focus();      
+    });
+    $(".btn-back,.overlay").click(function(){
+        $( ".navbar-nav li:first" ).removeClass( "hide" );
+        $( ".search-form" ).hide();
+    });				/*Bloqueo el bug del scroll*/	$( "#drawer-toggle,.btn-search,.overlay,.btn-back" ).click(function() {		$( "body" ).toggleClass( "block-scroll" );	});															
+});
+
